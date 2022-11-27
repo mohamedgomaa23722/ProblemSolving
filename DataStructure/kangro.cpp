b0776e48eadd4b44945171682053ca84d318e3a5
Mohamed Gomaa <62193989+mohamedgomaa23722@users.noreply.github.com>
2022-11-28 01:21:20 +0200

    Update kangro.cpp

--- DataStructure/kangro.cpp
+++ DataStructure/kangro.cpp
@@ -10,6 +10,26 @@
  */
  //Link : https://www.hackerrank.com/challenges/kangaroo/problem
 string kangaroo(int x1, int v1, int x2, int v2) {
+    if ((x1 > x2 && v1 > v2) || (x1 < x2 && v1 < v2)) return "NO";
+    
+    int fasterKangro = (x1 < x2&&v1 > v2)? x1:x2;
+    int fasterKangroV = (x1 < x2&&v1 > v2)? v1:v2;
+    int slowerKangro = (x1 == fasterKangro)? x2:x1;
+    int slowerKangroV = (x1 == fasterKangro)? v2:v1;
+
+    while (fasterKangro != slowerKangro) {
+        if (fasterKangro > slowerKangro) {
+            return "NO";
+        }
+        
+        fasterKangro +=fasterKangroV;
+        slowerKangro +=slowerKangroV;
+    }
+
+    return "YES";
+}
+//Answer optimization
+string kangaroo(int x1, int v1, int x2, int v2) {
     if(v1<=v2) return "NO";
     return ((x1-x2) % (v1-v2) == 0)? "YES":"NO";
 }
