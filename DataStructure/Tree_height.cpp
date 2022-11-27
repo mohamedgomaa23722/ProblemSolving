0565bfc0996ad5bcd94800927b1de581584bf494
Mohamed Gomaa <62193989+mohamedgomaa23722@users.noreply.github.com>
2022-11-27 23:54:39 +0200

    Create Tree_height.cpp

---
+++ DataStructure/Tree_height.cpp
@@ -0,0 +1,29 @@
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+
+//link:https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?isFullScreen=true
+
+    int height(Node* root) {
+        // Write your code here.
+        if(!root->left && !root->right){
+            return 0;
+        }
+        int leftLen=1;
+        int rightLen=1;
+
+        if(root->left){
+            leftLen+=height(root->left);
+        }
+        
+        if(root->right){
+            rightLen += height(root->right);
+        }
+        
+        return (leftLen > rightLen)? leftLen:rightLen;
+    }
