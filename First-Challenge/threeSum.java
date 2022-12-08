8f5474d6efc193c76601797b86a8088866e13fc5
Mohamed Gomaa <62193989+mohamedgomaa23722@users.noreply.github.com>
2022-12-08 20:30:45 +0200

    Create threeSum.java

---
+++ First-Challenge/threeSum.java
@@ -0,0 +1,26 @@
+ public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int high = nums.length - 1, low = i + 1, sum = -nums[i];
+                while (high > low) {
+                    if (nums[high] + nums[low] == sum) {
+                        result.add(Arrays.asList(nums[i],nums[low],nums[high]));
+
+                        while(low < high && nums[low] ==nums[low+1]) low++;
+                        while(low < high && nums[high] ==nums[high-1]) high--;
+
+                        low++;
+                        high--;
+                    } else if (nums[high] + nums[low] < sum) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
